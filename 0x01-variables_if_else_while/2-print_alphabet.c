e7875b57393bdad7f605618ba11d4170602909b4
Felipe Rivas <fe.rivasb@gmail.com>
2021-10-01 21:16:48 +0000

    task 2 attempt 1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	putchar (ch);
+	putchar (10);
+	return (0);
+}
