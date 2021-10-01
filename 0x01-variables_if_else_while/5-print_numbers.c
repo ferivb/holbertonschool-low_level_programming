28f1d8b2ee2e98a46d740aa85c3cd064e88079db
Felipe Rivas <fe.rivasb@gmail.com>
2021-10-01 22:23:38 +0000

    task 5 attempt 1

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	char n = 48;
+
+	for (n = 48; n <= 57; n++)
+	{
+	putchar(n);
+	}
+	putchar(10);
+	return (0);
+}
