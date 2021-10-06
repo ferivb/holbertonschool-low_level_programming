2328ee1e0e24a2e6cc88d51ed109995e31753648
Felipe Rivas <fe.rivasb@gmail.com>
2021-10-06 01:10:35 +0000

    task 10 ... is this a joke?

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * add - short description for add that no one will ever check
+  *
+  * @a: variable 1
+  * @b: variable 2
+  *
+  * Return: always 0
+  */
+int add(int a, int b)
+{
+	int c = a + b;
+
+	return (c);
+}
