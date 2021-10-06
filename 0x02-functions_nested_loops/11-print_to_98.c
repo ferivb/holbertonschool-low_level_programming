7aa5b7ad2286ddeafc2a1c35f8655777a3a000b1
Felipe Rivas <fe.rivasb@gmail.com>
2021-10-06 16:46:19 +0000

    task 11 attemp FINALLYYYYYga!

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <math.h>
+
+/**
+  * print_to_98 - Prints to 98
+  *
+  * @n: variable
+  */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		for (; n < 98; n++)
+		{
+			printf("%d, ",n);
+		}
+	}
+	else
+	{
+		for (; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	}
+	printf("98\n");
+}
