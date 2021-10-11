dc95bedf14dc057210e1a35774ff44980e3d1456
Felipe Rivas <fe.rivasb@gmail.com>
2021-10-11 21:56:10 +0000

    task 1 attempt 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * swap_int - Swaps two intergers
+  *
+  * @a: interger to swap with int b
+  * @b: interger to swap with int a
+  */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
