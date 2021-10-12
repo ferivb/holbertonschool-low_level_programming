b4415508ff80769579b1c7ed08eaaa62a23c56a0
Felipe Rivas <fe.rivasb@gmail.com>
2021-10-12 21:22:50 +0000

    task 8 attempt 1

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * print_array - Array to print
+  * @a: array
+  * @n: position
+  */
+void print_array(int *a, int n)
+{
+	int index;
+
+	for (index = 0; index < n; index++)
+	{
+		if (index == n - 1)
+		{
+			printf("%d\n", a[index]);
+		}
+		else
+		{
+			printf("%d, ", a[index]);
+		}
+	}
+}
