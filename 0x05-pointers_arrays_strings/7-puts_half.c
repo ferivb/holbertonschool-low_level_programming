f92c9ff19918675a3e691682dbd9bc8cde700191
Felipe Rivas <fe.rivasb@gmail.com>
2021-10-12 16:49:05 +0000

    task 7 attempt 1

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * puts_half - Puts the second calf of a string
+  *
+  * @str: string to cut in half
+  */
+void puts_half(char *str)
+{
+	int count, i;
+
+	for (count = 0; str[count]; count++)
+	{
+		continue;
+	}
+	if (count % 2 != 0)
+		count = count + 1;
+	i = count / 2;
+	for (; i < count; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar(10);
+}
