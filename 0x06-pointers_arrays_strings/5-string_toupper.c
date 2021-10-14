9793ef84f07ec27a2cf7bcb2f740cab7cb32604e
Felipe Rivas <fe.rivasb@gmail.com>
2021-10-14 19:12:58 +0000

    task 5 attempt 1

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * string_toupper - blablaba
+  * @a: blababla
+  * Return: blabalba
+  */
+char *string_toupper(char *a)
+{
+	int i;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		if (a[i] >= 97 && a[i] <= 122)
+		{
+			a[i] = a[i] - 32;
+		}
+	}
+	return (a);
+}
