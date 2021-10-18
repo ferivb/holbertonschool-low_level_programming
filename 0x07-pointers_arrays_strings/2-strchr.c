b162119cb4233b36ae1c2b1e8191f6090747b47c
Felipe Rivas <fe.rivasb@gmail.com>
2021-10-18 20:15:20 +0000

    task 2 attempt 1

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,4 +3,18 @@
 /**
   * _strchr - locates a char in a string
   *
+  * @c: char to search
+  * @s: return to this var
   *
+  * Return: The char found
+  */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+		{
+			return (s);
+		}
+	} while (s++);
+	return (0);
+}
