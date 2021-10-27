c9a0daee62eecf4f9b579387d0049bb901092ade
Felipe Rivas <fe.rivasb@gmail.com>
2021-10-27 01:04:42 +0000

    task 3 attempt 1

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+  * alloc_grid - allocates a grid
+  * @width: homonim
+  * @height: homonim
+  * Return: Grid
+  */
+int **alloc_grid(int width, int height)
+{
+	int **gridmem, i, j;
+
+	if (width < 1 || height < 1)
+	{
+		return (NULL);
+	}
+	gridmem = malloc(sizeof(int) * height);
+	for (i = 0; i < height; i++)
+	{
+		gridmem[i] = malloc(sizeof(int) * width);
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			if (gridmem == NULL)
+			{
+				return (NULL);
+			}
+			gridmem[i][j] = 0;
+		}
+	}
+	return (gridmem);
+	free(gridmem);
+}
