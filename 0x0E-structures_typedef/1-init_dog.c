b0e5965833017872decf999de20719c389f30a0d
Felipe Rivas <fe.rivasb@gmail.com>
2021-11-01 19:53:08 +0000

    task 1 attempt 2

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,11 +1,18 @@
 #include "dog.h"
 
 /**
-  *
+  * init_dog - initializes struct dog
+  * @d: address to initialize
+  * @name: dog's name
+  * @age: dog's age
+  * @owner: dog's owner
   */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
