94177daeb1142613cd74e6eec1341ffecab1b66b
Felipe Rivas <fe.rivasb@gmail.com>
2021-11-01 18:53:50 +0000

    task 0 attempt 1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * struct dog - enunciates dog info
+  *@name: name of the dog
+  *@age: age of the dog
+  *@owner: dog owner
+  */
+
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif
