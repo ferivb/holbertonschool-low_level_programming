21b343d475b763b9ed08a0ed1e44398833645fdc
Felipe Rivas <fe.rivasb@gmail.com>
2021-11-03 21:19:34 +0000

    task 0 attempt 2

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,11 @@
 #include "function_pointers.h"
 
 /**
-  *
+  * print_name - Prints a string using a pointer
+  * @name: name string.
+  * @f: funtion pointer
   */
 void print_name(char *name, void (*f)(char *))
 {
-	f (name);
+	f(name);
 }
