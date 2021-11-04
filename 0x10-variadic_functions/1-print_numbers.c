a5650c6b846a17cf69dd8b185580c17d8b1c1cb6
Felipe Rivas <fe.rivasb@gmail.com>
2021-11-04 17:38:48 +0000

    tasl 1 attempt 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,8 +1,28 @@
 #include "variadic_functions.h"
 
 /**
-  *
+  * print_numbers - print several numbers
+  * @separator: separator between the numbers
+  * @n: amount of numbers
   */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	unsigned int i = 0;
 
+	if (n)
+	{
+		va_list ap;
+
+		va_start(ap, n);
+		for (; i < (n - 1); i++)
+		{
+			if (separator)
+				printf("%d%s", va_arg(ap, unsigned int), separator);
+			else
+				printf("%d", va_arg(ap, unsigned int));
+		}
+		i++;
+		printf("%d\n", va_arg(ap, unsigned int));
+		va_end(ap);
+	}
+}
