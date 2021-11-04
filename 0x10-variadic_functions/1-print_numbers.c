f06356b2f91f7c03c59ba0aa224e19d481c3e0d8
Felipe Rivas <fe.rivasb@gmail.com>
2021-11-04 21:04:51 +0000

    task 1 attempt 3

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,7 +21,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			else
 				printf("%d", va_arg(ap, unsigned int));
 		}
-		printf("\n");
 		va_end(ap);
+		printf("\n");
 	}
 }
