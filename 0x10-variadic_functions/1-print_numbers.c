b0632913f62ef221a468f2eb3997c37c9a32ca9e
Felipe Rivas <fe.rivasb@gmail.com>
2021-11-04 21:09:18 +0000

    task 1 attempt 4

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -8,20 +8,16 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i = 0;
+	va_list ap;
 
-	if (n)
+	va_start(ap, n);
+	for (; i < n; i++)
 	{
-		va_list ap;
-
-		va_start(ap, n);
-		for (; i < n; i++)
-		{
-			if (separator && i != 0)
-				printf("%s%d", separator, va_arg(ap, unsigned int));
-			else
-				printf("%d", va_arg(ap, unsigned int));
-		}
-		va_end(ap);
-		printf("\n");
+		if (separator && i != 0)
+			printf("%s%d", separator, va_arg(ap, unsigned int));
+		else
+			printf("%d", va_arg(ap, unsigned int));
 	}
+	va_end(ap);
+	printf("\n");
 }
