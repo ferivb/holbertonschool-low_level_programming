ee20497cb2d1749d917a22e253c26bfce027022b
Felipe Rivas <fe.rivasb@gmail.com>
2021-11-17 16:00:54 +0000

    task 0 attempt 1

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+  * print_listint - prints a list of ints
+  * @h: head of the list
+  * Return: amount of nodes
+  */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
