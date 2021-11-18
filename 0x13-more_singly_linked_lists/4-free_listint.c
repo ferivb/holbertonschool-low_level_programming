9735ef79d81cef8b42f7cec37f6016ed2ac4d5fc
Felipe Rivas <fe.rivasb@gmail.com>
2021-11-18 03:32:58 +0000

    task 4 attempt 1

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+  * free_list - Function that fress a list
+  * @head: Main node
+  * Return: Head
+  */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+	listint_t *new;
+
+	temp = head;
+	while (temp != NULL)
+	{
+		new = temp->next;
+		free(temp);
+		temp = new;
+	}
+}
