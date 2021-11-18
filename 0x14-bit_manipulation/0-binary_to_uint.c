3d472eaa805aa96b93bceab0164f03706e6c544a
Felipe Rivas <fe.rivasb@gmail.com>
2021-11-18 22:52:47 +0000

    task 0 attempt 2

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,59 +1,27 @@
 #include "main.h"
 
 /**
-  * _slen - lenght of a string
-  * @num: string
-  * Return: lenght of a string
-  */
-unsigned int _slen(const char *num)
-{
-	unsigned int i = 0;
-
-	for (; num[i] != '\0'; i++)
-		continue;
-	return (i);
-}
-
-/**
-  * myPow - potenciación
-  * @base: base
-  * @exp: exponent
-  * Return: result
-  */
-unsigned int myPow(unsigned int base, unsigned int exp)
-{
-	unsigned int i;
-	unsigned int number = 1;
-
-	for (i = 0; i < exp; ++i)
-		number *= base;
-	return (number);
-}
-
-/**
-  * binary_to_uint - converts binary to decimal
-  * @b: string
+  * binary_to_uint - converts binary to unsigned int
+  * @b: string of 1s and 0s
   * Return: decimal number
   */
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int dec = 0;
-	int i = 0, count = 0;
+	int i = 0;
 
-	i = _slen(b);
-	if (b)
+	for (; b[i] != '\0'; i++)
 	{
-		for (i = i - 1; b[count]; count++, i--)
+		if (b[i] == '0' || b[i] == '1')
 		{
-			if (b[count] == '1')
-			{
-				dec = dec + myPow(2, i);
-			}
-			else if (b[count] != '0')
-			{
-				dec = 0;
-				return (0);
-			}
+			dec <<= 1;
+			dec += (b[i] - 48);
+		}
+		else
+		{
+			dec = 0;
+			i = 0;
+			return (dec);
 		}
 	}
 	return (dec);
