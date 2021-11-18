ef092a50d93ab22724d08491cbbc3aed83a72cc5
Felipe Rivas <fe.rivasb@gmail.com>
2021-11-18 03:40:04 +0000

    task 5 attempt 2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,6 +9,8 @@ void free_listint2(listint_t **head)
 	listint_t *temp;
 	listint_t *new;
 
+	if (head)
+	{
 	temp = *head;
 	while (temp != NULL)
 	{
@@ -17,4 +19,5 @@ void free_listint2(listint_t **head)
 		temp = new;
 	}
 	*head = NULL;
+	}
 }
