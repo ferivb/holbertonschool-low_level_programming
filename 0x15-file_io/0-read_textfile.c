418d208cd537837d453177af2870516178918285
Felipe Rivas <fe.rivasb@gmail.com>
2021-11-24 03:21:16 +0000

    task 0 attempt 1

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * read_textfile - prints a txt to the stdout
+  * @filename: name of the file
+  * @letters: letters to read
+  * Return: conter of numbers
+  */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t sizrd, sizwr;
+	int fd = open(filename, O_RDONLY);
+	char *hold = (char *)malloc(letters * sizeof(char));
+
+	if (hold == NULL)
+	{
+		free(hold);
+		fd = 0;
+		return (0);
+	}
+	sizrd = read(fd, hold, letters);
+	sizwr = write(STDOUT_FILENO, hold, sizrd);
+	free(hold);
+	close(fd);
+	fd = 0;
+	return (sizwr);
+}
