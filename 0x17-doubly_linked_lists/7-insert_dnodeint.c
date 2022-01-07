4c55fba2f0176dec2b8a4e518083f6d9b61aa944
Felipe Rivas <fe.rivasb@gmail.com>
2022-01-07 18:34:36 -0500

    task 7 attempt 1

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,13 @@
 #include "lists.h"
 
+/**
+ * insert_dnodeint_at_index - inserts a node at a given index
+ *
+ * @h: head of the list
+ * @idx: index to ad the list
+ * @n: data to add
+ * Return: pointer to the newly added node or NULL if failed
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new = NULL;
@@ -7,26 +15,33 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0;
 
 	old = *h;
-	while (i < idx)
+	if (idx == 0)
+		new = add_dnodeint(h, n);
+	else
 	{
-		if (old == NULL)
+		while (old != NULL)
 		{
-			return (NULL);
+			if (i == idx)
+			{
+				if (old->next == NULL)
+					new = add_dnodeint_end(h, n);
+				else
+				{
+					new = malloc(sizeof(dlistint_t));
+					if (new != NULL)
+					{
+						new->n = n;
+						new->next = old;
+						new->prev = old->prev;
+						old->prev = new;
+						old = new->prev;
+						old->next = new;
+					}
+				}
+			}
+			old = old->next;
+			i++;
 		}
-		old = old->next;
-		i++;
 	}
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-	{
-		return (NULL);
-	}
-	new->n = n;
-	new->next = old;
-	new->prev = old->prev;
-	old->prev = new;
-	old = new->prev;
-	old->next = new;
-
 	return (new);
 }
