cc25478ad577b7fd34ac685faa9fcb04214c66e1
Felipe Rivas <fe.rivasb@gmail.com>
2022-01-07 11:42:42 -0500

    task 5 (next day) attempt 1

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -7,14 +7,13 @@
  */
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *temp;
-	dlistint_t *new;
+	dlistint_t *temp = NULL;
 
 	temp = head;
 	while (head != NULL)
 	{
-		new = temp->next;
+		temp = head;
+		head = head->next;
 		free(temp);
-		temp = new;
 	}
 }
