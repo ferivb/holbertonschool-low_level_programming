6b8a2ea4851965e5bd45f7c8720de9e0c52885c2
Felipe Rivas <fe.rivasb@gmail.com>
2022-01-28 14:48:32 -0500

    task 3 collision handling correction

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -48,7 +48,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		collision = ht->array[index];
 		new->key = strdup(key);
 		new->value = strdup(value);
-		new->next = NULL;
+		new->next = collision;
 		ht->array[index] = new;
 		return (1);
 	}
