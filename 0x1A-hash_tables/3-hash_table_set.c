f76adb0251ad2f539a72f0eeba5d952ad90eb416
Felipe Rivas <fe.rivasb@gmail.com>
2022-01-28 19:11:45 -0500

    task 3 test - (No Betty)

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,43 +14,53 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index = 0;
 	hash_node_t *collision = NULL, *new = NULL;
 
-	if (key == NULL)
-		return 0;
-
-	/* Generates the hash(index) for the key */
-	index = key_index((const unsigned char *)key, ht->size);
-
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
+	if (key && key[0] != '\0' && ht && ht->array && ht->size)
 	{
-		free(new);
-		return (0);
-	}
+		/* Generates the hash(index) for the key */
+		index = key_index((const unsigned char *)key, ht->size);
 
-	/* If the hash index is empty */
-	if (ht->array[index] == NULL)
-	{
-		new->key = strdup(key);
-		new->value = strdup(value);
-		new->next = NULL;
-		ht->array[index] = new;
-		return (1);
-	}
-	/* Collission handling */
-	else
-	{
-		collision = malloc(sizeof(hash_node_t));
-		if (collision == NULL)
+		new = malloc(sizeof(hash_node_t));
+		if (new == NULL)
 		{
-			free(collision);
+			free(new);
 			return (0);
 		}
-		collision = ht->array[index];
-		new->key = strdup(key);
-		new->value = strdup(value);
-		new->next = collision;
-		ht->array[index] = new;
-		return (1);
+
+		/* If the hash index is empty */
+		if (ht->array[index] == NULL)
+		{
+			new->key = strdup(key);
+			new->value = strdup(value);
+			new->next = NULL;
+			ht->array[index] = new;
+			return (1);
+		}
+		/* Collission handling */
+		else
+		{
+			collision = malloc(sizeof(hash_node_t));
+			if (collision == NULL)
+			{
+				free(collision);
+				return (0);
+			}
+			collision = ht->array[index];
+			while (collision != NULL)
+			{
+				if (strcmp(collision->key, key) == 0)
+				{
+					collision->value = strdup(value);
+					return (1);
+				}
+				collision = collision->next;
+			}
+			collision = ht->array[index];
+			new->value = strdup(value);
+			new->key = strdup(key);
+			new->next = collision;
+			ht->array[index] = new;
+			return (1);
+		}
 	}
 	return (0);
 }
--- 0x1A-hash_tables/3-main.c
+++ 0x1A-hash_tables/3-main.c
@@ -11,8 +11,33 @@
 int main(void)
 {
     hash_table_t *ht;
+	hash_node_t *node;
+	unsigned long int i = 0;
+	unsigned long int index = 0;
+	char *temp = NULL;
 
     ht = hash_table_create(1024);
     hash_table_set(ht, "betty", "cool");
+	hash_table_set(ht, "hetairas", "This should print second");
+	hash_table_set(ht, "mentioner", "This should print first");
+	hash_table_set(ht, "empty", "");
+	hash_table_set(ht, "depravement", "You shouldn't see this");
+	hash_table_set(ht, "depravement", "Override");
+
+	while (i < 1024)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			temp = node->key;
+			index = key_index((const unsigned char *)temp, ht->size);
+			printf("-----array[i] = %li ------\n", i);
+			printf("Key: %s   Hash: %li\n pair->\t\t", node->key, index);
+			printf("%s - %s\n\n", node->key, node->value);
+			node = node->next;
+		}
+		i++;		
+	}
+	
     return (EXIT_SUCCESS);
 }
